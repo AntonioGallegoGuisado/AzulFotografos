89c200b9387ceed00d1fbfa0a5522b1c2449db6a
AntonioGallegoGuisado <qebranto@gmail.com>
2016-05-12 17:16:43 +0200

    Configuracion bootstrap correcta

--- app/models/foto.rb
+++ app/models/foto.rb
@@ -1,7 +1,7 @@
 class Foto < ActiveRecord::Base
   belongs_to :galery
   
-   has_attached_file :image, styles: { small: "640x640", med: "1000x1000", large: "2000x2000" }
+   has_attached_file :image, styles: { small: "64x64", med: "300x300", large: "500x500" }
    validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
   
   def to_s
