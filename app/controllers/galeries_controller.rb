9fee689ff95ca5ff8b2a41b42366280f36cee69e
AntonioGallegoGuisado <qebranto@gmail.com>
2016-05-19 13:54:43 +0200

    Correcciones menores en galerias

--- app/controllers/galeries_controller.rb
+++ app/controllers/galeries_controller.rb
@@ -1,10 +1,12 @@
 class GaleriesController < ApplicationController
+  
+  @@rutaGeneral="localhost:3000/"
+  
   def index
     
     @galerias=Array.new
     #Filtramos galerias por visibles
     galerias=Galery.order('Galeries.posicion').where(:visible => true)
-    
     
     #Por cada registro (llamado gal) en galerias:    
     for gal in galerias
@@ -15,13 +17,12 @@ class GaleriesController < ApplicationController
       galeria=Array.new
       #Obtenemos todas las fotos relacionadas con la galeria
       #filtramos por visibles las fotos
-      fotos=Foto.order('Fotos.posicion').where(:galery_id => gal.id).where(:visible => true)
+      #limitamos cantidad de fotos por galeria a 3
+      fotos=Foto.order('Fotos.posicion').where(:galery_id => gal.id).where(:visible => true).limit(3)
       for foto in fotos
         #introducimos cada foto como un objeto independiente en un array galeria
-        #TODO: limitar cantidad de fotos por galeria
         galeria << foto
-        #TODO: crear los textos alternativos de cada foto
-        #TODO: enlazar cada foto con su enlace al controlador de fotos
+        
       end
       #introducimos cada array galeria como un objeto independiente en la variable de la vista
       #introducimos tambien su titulo y descripcion
