5fd849e57a706db3df82812836381fbcda7cc207
AntonioGallegoGuisado <qebranto@gmail.com>
2016-05-20 19:00:47 +0200

    Comit anterior incompleto, completo aqui

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,7 @@
 class Category < ActiveRecord::Base
+
+   has_attached_file :image, styles: { small: "64x64", med: "300x300", large: "500x500" }
+   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
   
   def to_s
     self.titulo
