c9f48053a503d04ce2e3530b310c60af80a8f6aa
AntonioGallegoGuisado <qebranto@gmail.com>
2016-05-24 17:34:18 +0200

    Añadida presentacion a cada galeria

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   get '/galerias/:id' => 'galeries#index'
   get '/galeria/:id'=> 'galeries#galeria'
   get '/galeria/foto/:id' => 'fotos#index'
+  get '/galeria/presentacion/:id' =>'galeries#presentacion'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
