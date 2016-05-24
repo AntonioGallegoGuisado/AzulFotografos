b8e0ac2af58a98494ffe2969927b501812cfd483
AntonioGallegoGuisado <qebranto@gmail.com>
2016-05-24 13:02:31 +0200

    Redimension presentacion y mejora de rendimiento

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,12 +1,8 @@
 class CategoriesController < ApplicationController
   def index
     
-    #TODO: las categorias mostraran 1 sola foto y el nombre de la categoria encima
-    
     #obtenemos las categorias visibles y las ordenamos
     @categorias=Category.order('Categories.posicion').where(:visible => true)
-    
-    #TODO: enlaces en la vista y en el header
      
   end
 end
