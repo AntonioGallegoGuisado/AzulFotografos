5bca4c47270b771f7002a782e2097201639cb121
AntonioGallegoGuisado <qebranto@gmail.com>
2017-01-26 19:58:28 +0100

    correccion vulnerabilidad de inspeccion de codigo

--- app/assets/javascripts/protect.js
+++ app/assets/javascripts/protect.js
@@ -1,6 +1,16 @@
 	var isNS = (navigator.appName == "Netscape") ? 1 : 0;
 	
 	if(navigator.appName == "Netscape") document.captureEvents(Event.MOUSEDOWN||Event.MOUSEUP);
+
+		
+	$(document).keydown(function(event){
+    if(event.keyCode==123){
+    return false;
+   }
+	else if(event.ctrlKey && event.shiftKey && event.keyCode==73){        
+      return false;  //Prevent from ctrl+shift+i
+   }
+});
 	
 	document.ondragstart= function(){return false};
 	document.onselectstart= function(){return false};
@@ -45,12 +55,3 @@
 	document.oncontextmenu = mischandler;
 	document.onmousedown = mousehandler;
 	document.onmouseup = mousehandler;
-	
-	$(document).keydown(function(event){
-    if(event.keyCode==123){
-    return false;
-   }
-	else if(event.ctrlKey && event.shiftKey && event.keyCode==73){        
-      return false;  //Prevent from ctrl+shift+i
-   }
-});
