0a7a90106bff2f437a681a1a45022ace1f679717
AntonioGallegoGuisado <qebranto@gmail.com>
2017-01-26 17:11:17 +0100

    Prevent F12 and ctrl+shift+i (inspect code)

--- app/assets/javascripts/protect.js
+++ app/assets/javascripts/protect.js
@@ -14,3 +14,12 @@
 	document.oncontextmenu = mischandler;
 	document.onmousedown = mousehandler;
 	document.onmouseup = mousehandler;
+	
+	$(document).keydown(function(event){
+    if(event.keyCode==123){
+    return false;
+   }
+	else if(event.ctrlKey && event.shiftKey && event.keyCode==73){        
+      return false;  //Prevent from ctrl+shift+i
+   }
+});
