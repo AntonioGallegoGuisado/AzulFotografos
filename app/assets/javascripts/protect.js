791356563d5d95acf29e85864bfabc7269e4981f
AntonioGallegoGuisado <qebranto@gmail.com>
2017-01-26 19:51:25 +0100

    Correcion bug mozilla

--- app/assets/javascripts/protect.js
+++ app/assets/javascripts/protect.js
@@ -5,6 +5,34 @@
 	document.ondragstart= function(){return false};
 	document.onselectstart= function(){return false};
 	
+	function disableselect(e) {             
+          return false 
+      } 
+      function reEnable() { 
+          return true 
+      } 
+
+      document.onselectstart = new Function("return false") 
+
+      if (window.sidebar) { 
+          document.onmousedown = disableselect                    // for mozilla           
+          document.onclick = reEnable 
+      } 
+
+      function clickIE() { 
+          if (document.all) { 
+              (message); 
+              return false; 
+          } 
+      } 
+
+
+      document.oncontextmenu = new Function("return false") 
+
+      var element = document.getElementById('tbl'); 
+
+      element.onmousedown = function () { return false; }        // For Mozilla Browser
+	
 	function mischandler(){
 	return false;
 	}
